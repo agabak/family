20f0fdd54c37eaf63e597da91f733f788915139b
Agaba Kabunga <Agaba Kabunga>
2017-09-23 15:49:42 -0500

    change the server config

--- build/index.js
+++ build/index.js
@@ -1,5 +1,24 @@
 
 var express = require('express'),
+bodyParser = require('body-parser'),
+path = require('path'),
+root = '/',
+port = process.env.Port || 3000,
+app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(root, 'build')));
+
+//serving api routes
+app.get('*', (req, res) => {
+res.sendFile('build/index.html', { root });
+})
+app.listen(port, () => console.log('API running on localhost:${port}'));
+
+
+/*
+var express = require('express'),
 path = require('path'),
 fs = require('fs');
 
@@ -31,55 +50,4 @@ fs.createReadStream(staticRoot + 'index.html').pipe(res);
 app.listen(app.get('port'), function() {
 console.log('app running on port', app.get('port'));
 });
-
-
-
-
-
-
-/*
-var express = require('express'),
-bodyParser = require('body-parser'),
-path = require('path'),
-root = '/',
-port = process.env.Port || 3000,
-app = express(),
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(root, 'build')));
-
-//serving api routes
-
-app.get('*', (req, res) => {
-res.sendFile('build/index.html', { root });
-})
-app.listen(port, () => console.log('API running on localhost:${port}'));
-
-
-
-/*
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    path = require('path'),
-    root = '/',
-    port = process.env.Port || 3000,
-    app = express();
-
-//  connection cli
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://mtomawe-db:awgtNvUx2xLGHSkgzVuOEHosWQE3wUKFwElCxJggrwpGR8G2GasUpq09qJT5pJyPkZKvVfOq6KF19sz0sZM88g==@mtomawe-db.documents.azure.com:10255/?ssl=true';
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(root, 'build')));
-
-app.get('*', (req, res) => {
-    res.sendFile('build/index.html', { root });
-})
-app.listen(port, () => console.log('API running on localhost:${port}'));
 */
