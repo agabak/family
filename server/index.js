45861e5065d36dc10bff722216358c8e5eba1515
Agaba Kabunga <Agaba Kabunga>
2017-09-23 16:18:23 -0500

    server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,38 @@
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs');
+
+var app = express();
+var staticRoot = __dirname + '/';
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(staticRoot));
+
+app.use(function (req, res, next) {
+
+    // if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if (accept !== 'html') {
+        return next();
+    }
+
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== '') {
+        return next();
+    }
+
+    fs.createReadStream(staticRoot + 'index.html').pipe(res);
+
+});
+
+app.listen(app.get('port'), function () {
+    console.log('app running on port', app.get('port'));
+});
+
+
+/*
 
 var express = require('express'),
 bodyParser = require('body-parser'),
@@ -15,39 +50,4 @@ app.get('*', (req, res) => {
 res.sendFile('build/index.html', { root });
 })
 app.listen(port, () => console.log('API running on localhost:${port}'));
-
-
-/*
-var express = require('express'),
-path = require('path'),
-fs = require('fs');
-
-var app = express();
-var staticRoot = __dirname + '/';
-
-app.set('port', (process.env.PORT || 3000));
-
-app.use(express.static(staticRoot));
-
-app.use(function(req, res, next){
-
-// if the request is not html then move along
-var accept = req.accepts('html', 'json', 'xml');
-if(accept !== 'html'){
-    return next();
-}
-
-// if the request has a '.' assume that it's for a file, move along
-var ext = path.extname(req.path);
-if (ext !== ''){
-    return next();
-}
-
-fs.createReadStream(staticRoot + 'index.html').pipe(res);
-
-});
-
-app.listen(app.get('port'), function() {
-console.log('app running on port', app.get('port'));
-});
 */
